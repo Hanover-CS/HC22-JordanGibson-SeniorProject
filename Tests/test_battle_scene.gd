5fe35f2584c3c9fbe3a14241977919d10213b840
jordangibson1 <gibsonj22@hanover.edu>
2021-12-20 21:52:28 -0500

    - Messing with tests, Orphans
    - Messing with tests
       - Tried to make a method to clean up
         each test instance
         - Should be changed to after_each
            rather than before_each
    - Orphans
       - Instancing is making lost child
         nodes with no parent
         - Need to figure out how to  delete
           them

--- Tests/test_battle_scene.gd
+++ Tests/test_battle_scene.gd
@@ -7,15 +7,35 @@ class TestTurnQueue:
 	onready var skullflame_scene = preload("res://Enemies/Small/Skullflame/Skullflame.tscn")
 	onready var wisp_scene = preload("res://Enemies/Small/Wisp/Whisp.tscn")
 	onready var glutmon_scene = preload("res://Enemies/Bosses/Glutmon/Glutmon.tscn")
+	
 	onready var player_scene = preload("res://Player/Player.tscn")
 	onready var battle_scene = preload("res://Battle/Battle.tscn")
 	
+	onready var player = player_scene.instance()
+	onready var battle = battle_scene.instance()
+	
+	
+	onready var char_parent = battle.get_node("Characters")
+	
+	func before_each():
+		var curr_child
+		for i in range(0, char_parent.get_child_count(), -1):
+			curr_child = char_parent.get_child(i)
+			char_parent.remove_child(curr_child)
+	
 	func test_small_enemy_order():
-		var player = player_scene.instance()
 		var imp = imp_scene.instance()
-		var battle = battle_scene.instance()
-		var char_parent = battle.get_node("Characters")
 		char_parent.add_child(imp)
 		char_parent.add_child(player)
 		battle.create_turn_order()
 		assert_eq(char_parent.get_child(0), imp)
+
+	func test_small_enemy_order_multiple():
+		var imp = imp_scene.instance()
+		var skullflame = skullflame_scene.instance()
+		skullflame.speed = 2
+		char_parent.add_child(imp)
+		char_parent.add_child(player)
+		char_parent.add_child(skullflame)
+		battle.create_turn_order()
+		assert_eq(char_parent.get_children(), [imp, skullflame, player])
