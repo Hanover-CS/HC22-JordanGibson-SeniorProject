984576d4afae3b48d8a2c89ba481095e4e19606c
jordangibson1 <gibsonj22@hanover.edu>
2021-12-27 22:02:00 -0500

    Player Movement, World Scene
    - Player Movement
       - Added player movement functionality
    - World Scene
       - Added enemy and player to world scene to test battle

--- Player/Player.gd
+++ Player/Player.gd
@@ -4,6 +4,10 @@ export (int) var hp = 10
 export (int) var damage = 1
 export (int) var defense = .05
 export (int) var speed = 1
+export (int) var movement_speed = 100
+
+var direction : Vector2
+var screen_size = 1024
 
 signal player_attack(damage)
 signal player_death()
@@ -14,10 +18,20 @@ func _ready():
 	player_animation.play("Idle")
 
 func _physics_process(delta):
-	if Input.is_action_just_pressed("ui_accept"):
-		emit_signal("player_attack", damage)
-		player_animation.play("Slash")
-		player_animation.queue("Idle")
+	direction.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	direction.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	
+	global_position.x += direction.x * movement_speed * delta
+	global_position.y += direction.y * movement_speed * delta
+	
+	if direction.x != 0 or direction.y != 0:
+		if direction.x < 0:
+			$Sprite.flip_h = true
+		else:
+			$Sprite.flip_h = false
+		player_animation.play("Walk")
+	else:
+		player_animation.play("Idle")
 
 func _on_enemy_attack(damage):
 	hp -= damage
